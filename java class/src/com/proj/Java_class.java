f5fb77c7147945b915f92cbc25082e234587ca94
DELL <DELL@SHANKAR>
2022-07-05 16:49:52 +0530

    varadha

---
+++ java class/src/com/proj/Java_class.java
@@ -0,0 +1,22 @@
+package com.proj;
+
+public class Java_class {
+	
+	
+	public void java1() {
+		int d=1;
+		String g="java";
+		System.out.println(d);
+		System.out.println(g);
+
+	}
+	
+	public static void main(String[] args) {
+		
+		Java_class sh = new Java_class();
+		
+		sh.java1();
+		
+	}
+	
+}
